842ff346aea9d09e6c8e772e7e244aca50a87760
Gufinger07 <gusti.cf@gmail.com>
2021-03-15 21:18:24 -0300

    Começando o projeto

---
+++ js/slick/main.js
@@ -0,0 +1,11 @@
+$(function() {
+    $('.imagens').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true
+      });
+})
+
